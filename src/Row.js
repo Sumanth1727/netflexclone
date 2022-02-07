3034dacfbd42b3430f11a4df71edc5fddbd79b9f
Sumanth Pasupuleti <73636498+Sumanth1727@users.noreply.github.com>
2022-02-07 15:52:48 -0500

    added video player support

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import YouTube from "react-youtube";
 import instance from "./axiosrequest";
 import "./Row.css"
+import movieTrailer from "movie-trailer";
 
 const poster_base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchurl,isposter  }) {
   const [movies, setmovies] = useState([]);
+  const [trailerurl, settrailerurl] = useState("");
+
+  const handleclick=(movie)=>{
+    console.log(movie)
+    if (trailerurl){
+      settrailerurl("");
+
+    }
+    else{
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+      .then((url )=>{
+        console.log(url)
+        const urlParams= new URLSearchParams(new URL(url).search);
+        settrailerurl(urlParams.get("v"))
+
+      })
+      .catch((error) => console.log(error));
+    }
+
+  }
+
+
   useEffect(() => {
     async function fetchdata() {
       const request = await instance.get(fetchurl);
@@ -19,6 +43,14 @@ function Row({ title, fetchurl,isposter  }) {
   }, [fetchurl]);
 
   // {isposter} ? movie.poster_path: movie.backdrop_path
+  const opts={
+    height:"390",
+    width:"100%",
+    playerVars:{
+      autoplay:1,
+    },
+
+  };
 
   console.log(isposter)
 
@@ -30,6 +62,7 @@ function Row({ title, fetchurl,isposter  }) {
         {/* posters */} 
         {movies.map((movie) => (
           <img className={`row_poster ${isposter && "row_posterlarge"}`}
+          onClick={()=>handleclick(movie)}
           key={movie.id}
             src={`${poster_base_url}${isposter ? movie.poster_path: movie.backdrop_path}`}
             // src={poster_base_url + ({isposter} ) }
@@ -39,6 +72,7 @@ function Row({ title, fetchurl,isposter  }) {
           ></img>
         ))}
       </div>
+      {trailerurl && <YouTube videoId={trailerurl} opts={opts} />}
     </div>
   );
 }
